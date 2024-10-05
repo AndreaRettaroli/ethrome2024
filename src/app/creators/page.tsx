6246ff9ea89e5425cef098cdb47b253032f52d11
AndreaRettaroli <andrea.rettaroli@studio.unibo.it>
2024-10-05 19:38:29 +0200

    wip: migrated subscription into creators

--- src/app/creators/page.tsx
+++ src/app/creators/page.tsx
@@ -1,73 +1,83 @@
 "use client";
 
-import {
-  Section,
-  Cell,
-  Image,
-  List,
-  Placeholder,
-  Button,
-  Tabbar,
-} from "@telegram-apps/telegram-ui";
+import { IExecDataProtectorSharing } from "@iexec/dataprotector";
+import { useQuery } from "@tanstack/react-query";
+import { WalletClient } from "viem";
+import { useWalletClient } from "wagmi";
 
-import { useAccount, useConnect, useDisconnect } from "wagmi";
-import Sub from "@/components/Sub/Sub";
-import CreateButton from "@/components/Sub/Create";
-import { injected } from "wagmi/connectors";
-import { TabbarItem } from "@telegram-apps/telegram-ui/dist/components/Layout/Tabbar/components/TabbarItem/TabbarItem";
-import Link from "next/link";
-
-const tabs = ["profile", "creators"];
-
-export default function Creators() {
-  const { address, chain } = useAccount();
-  const { connect } = useConnect();
-  const { disconnect } = useDisconnect();
+export default function Subscriptions() {
+  const { data: wallet } = useWalletClient();
+  const {
+    isLoading,
+    isSuccess,
+    data: firstTenAccounts,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["allCreators"],
+    queryFn: async () => {
+      const dataProtectorSharing = new IExecDataProtectorSharing(wallet);
+      const { collectionOwners } =
+        await dataProtectorSharing.getCollectionOwners({
+          limit: 8,
+        });
+      return collectionOwners;
+    },
+  });
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      {address ? (
-        <>
-          <div className="grow">
-            <p>creators</p>
-          </div>
-          <div>
-            <Tabbar>
-              {tabs.map((tab) => (
-                <Link href={`/${tab}`}>
-                  <TabbarItem text={tab} onClick={() => "ciao"} />
-                </Link>
-              ))}
-            </Tabbar>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="grow">
-            <Placeholder
-              action={
-                <Button
-                  size="l"
-                  stretched
-                  onClick={() => connect({ connector: injected() })}
-                >
-                  Connect
-                </Button>
-              }
-              description="Join the best Telegram VIP betting community"
-              header="Telegram VIP Betting"
-            >
-              <img
-                width={100}
-                height={100}
-                alt="Telegram sticker"
-                className="blt0jZBzpxuR4oDhJc8s"
-                src="https://xelene.me/telegram.gif"
-              />
-            </Placeholder>
-          </div>
-        </>
+    <div>
+      <h1>Subscriptions</h1>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error: {error.message}</p>}
+      {isSuccess && (
+        <ul>
+          {firstTenAccounts.map((owner: any) => (
+            <li key={owner.id}>
+              <p>{owner.id}</p>
+              <Collections owner={owner.id as `0x${string}`} />
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
+
+  function Collections({ owner }: { owner: `0x${string}` }) {
+    const { data: wallet } = useWalletClient();
+    const {
+      isLoading,
+      isSuccess,
+      data: allCollections,
+      isError,
+      error,
+    } = useQuery({
+      queryKey: ["allCollections-" + owner],
+      queryFn: async () => {
+        const dataProtectorSharing = new IExecDataProtectorSharing(wallet);
+        const { collections } =
+          await dataProtectorSharing.getCollectionsByOwner({
+            owner,
+          });
+        return collections;
+      },
+    });
+
+    return (
+      <div>
+        <h1>Collections</h1>
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Error: {error.message}</p>}
+        {isSuccess && (
+          <ul>
+            {allCollections.map((collection: any) => (
+              <li key={collection.id}>
+                {collection.id} {collection.creationTimestamp.toString()}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
